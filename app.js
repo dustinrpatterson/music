fec59eb7061208ec2ba433bdc61f223a4def55c8
Dustin Patterson <dustinrpatterson@gmail.com>
2016-09-27 19:00:20 -0600

    it works!

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 //Do Not Modify the getMusic function
-function getMusic(){
-  var artist = document.getElementById('artist').value;
-  itunes.getMusicByArtist(artist).then(drawSongs);
+function getMusic() {
+    var artist = document.getElementById('artist').value;
+    itunes.getMusicByArtist(artist).then(drawSongs);
 }
 
 
 
-function drawSongs(songList){
- var template = "";
- var songElement = document.getElementById('foundMusic')
- for (var i = 0; i < songList.length; i++) {
-   var foundMusic = songList[i]
-   template += `<div id="foundMusic">
+function drawSongs(songList) {
+    var template = "";
+    var songElement = document.getElementById('foundMusic')
+    for (var i = 0; i < songList.length; i++) {
+        var foundMusic = songList[i]
+        template += `<div class=container>
+  <div class="row">
+    <div id="foundMusic" class="col-xs-6 card">
+      <div class="row">
+        <div class="col-md-3">
         <div class="img-container">
           <img src="${foundMusic.albumArt}" alt="" />
+          </div>
         </div>
-        <div class="text-container">
-          <h2>${foundMusic.title}</h2>
+        <div class="col-md-offset-3 music">
+          <h4>${foundMusic.title}</h4>
           <p>${foundMusic.artist}</p>
           <p>${foundMusic.collection}</p>
-          <p>${foundMusic.price}</p>
-          
+          <p>Price ${foundMusic.price}</p>
           <audio controls>
-          <source src="${foundMusic.preview}" >
-        Your browser does not support the audio element.
-        </audio>
+            <source src="${foundMusic.preview}"> Your browser does not support the audio element.
+          </audio>
         </div>
-      </div>`
-      songElement.innerHTML = template
- }
- 
- 
- 
-  console.log(songList);
+      </div>
+    </div>
+  </div>
+</div>`
+        songElement.innerHTML = template
+    }
+
+    console.log(songList);
 }
